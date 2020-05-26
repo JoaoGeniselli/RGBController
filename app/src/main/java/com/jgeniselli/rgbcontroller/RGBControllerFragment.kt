f8a8c399b4ceea2fd6cca8946f77cda8dd9950b0
JoaoGeniselli <joao.g.geniselli@gmail.com>
2020-05-26 12:34:49 -0300

    Adjustments.

--- app/src/main/java/com/jgeniselli/rgbcontroller/RGBControllerFragment.kt
+++ app/src/main/java/com/jgeniselli/rgbcontroller/RGBControllerFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import kotlinx.android.synthetic.main.rgb_controller_fragment.*
 import org.koin.android.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
 
@@ -14,6 +15,10 @@ class RGBControllerFragment : Fragment() {
     private val args: RGBControllerFragmentArgs by navArgs()
     private val viewModel by viewModel<RGBControllerViewModel> { parametersOf(args.deviceAddress) }
 
+    private val red: Int get() = seek_bar_red.progress
+    private val green: Int get() = seek_bar_green.progress
+    private val blue: Int get() = seek_bar_blue.progress
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -21,12 +26,9 @@ class RGBControllerFragment : Fragment() {
         return inflater.inflate(R.layout.rgb_controller_fragment, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-    }
-
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        button_apply.setOnClickListener { viewModel.onApplyClicked(red, green, blue) }
     }
 
 }
